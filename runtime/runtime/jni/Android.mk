08bc5794f781cb6ac726abcdb2375a05d66bf189
0Dex <163450531+MSR-Cracker@users.noreply.github.com>
2026-08-12 03:23:32 +0300

    Add Android.mk for native library build configuration

---
+++ runtime/runtime/jni/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := native_library
+
+LOCAL_SRC_FILES := \
+    ../cpp/runtime.cpp
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/../cpp
+
+LOCAL_CPPFLAGS := \
+    -std=c++17 \
+    -fvisibility=hidden
+
+LOCAL_LDLIBS := \
+    -llog
+
+include $(BUILD_SHARED_LIBRARY)
